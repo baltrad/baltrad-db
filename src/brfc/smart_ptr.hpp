118baf1685ddb6ae31a4d968e71dc1a330e941a0
Sander Ernes <sander.ernes@emhi.ee>
2010-01-06 13:17:09 +0200

    add std::auto_ptr to smart pointers

--- src/brfc/smart_ptr.hpp
+++ src/brfc/smart_ptr.hpp
@@ -1,8 +1,11 @@
 #include <boost/shared_ptr.hpp>
 #include <boost/enable_shared_from_this.hpp>
 
+#include <memory>
+
 namespace brfc {
 
+using std::auto_ptr;
 using boost::shared_ptr;
 using boost::enable_shared_from_this;
 using boost::const_pointer_cast;
