438d288d123e60112c1d53ff56855b73df72d7e1
Sander Ernes <sander.ernes@emhi.ee>
2010-01-13 12:39:20 +0200

    change Query tests to use more diverse data

--- test/RDB_Query_test.cpp
+++ test/RDB_Query_test.cpp
@@ -15,42 +15,75 @@
 #include <QtCore/QDate>
 #include <QtCore/QTime>
 
+#include <algorithm>
+#include <vector>
+
 using namespace brfc;
 
 #ifdef BRFC_TEST_DB_DSN
 namespace {
 
+/**
+ * test data for this fixture:
+ *
+ * src name obj  date       time  xsize ysize
+ *   1 td1  PVOL 2000-01-01 12:00     1     2
+ *   2 td2  PVOL 2000-01-01 12:01     2     2
+ *   1 td3  PVOL 2000-01-01 12:02     3
+ *                                    3
+ *   2 td4  CVOL 2001-01-01 12:00     6     4
+ *                                          5
+ *   1 td5  SCAN 2002-02-01 12:00     5     2
+ *                                    2     5
+ */
 struct RDB_Query_test : public testing::Test {
     RDB_Query_test()
             : xpr()
-            , src("WMO:02606,RAD:SE50,PLC:Ängelholm")
+            , src1("WMO:02606,RAD:SE50,PLC:Ängelholm")
+            , src2("WMO:02666,RAD:SE51,PLC:Karlskrona")
             , db(BRFC_TEST_DB_DSN)
-            , td1("PVOL", QDate(2000, 1, 1), QTime(12, 0), src)
-            , td2("PVOL", QDate(2000, 1, 1), QTime(12, 1), src)
-            , td3("PVOL", QDate(2000, 1, 1), QTime(12, 2), src)
+            , td1("PVOL", QDate(2000, 1, 1), QTime(12, 0), src1)
+            , td2("PVOL", QDate(2000, 1, 1), QTime(12, 1), src2)
+            , td3("PVOL", QDate(2000, 1, 1), QTime(12, 2), src1)
+            , td4("CVOL", QDate(2001, 1, 1), QTime(12, 0), src2)
+            , td5("SCAN", QDate(2002, 2, 1), QTime(12, 0), src1)
             , query(&db) {
-        td1.data_object("/bla", true).add_attribute("where/xsize", QVariant(static_cast<int64_t>(1)));
-        td1.data_object("/bla", true).add_attribute("where/ysize", QVariant(static_cast<int64_t>(2)));
-
-        td2.data_object("/bla", true).add_attribute("where/xsize", QVariant(static_cast<int64_t>(2)));
-        td2.data_object("/bla", true).add_attribute("where/ysize", QVariant(static_cast<int64_t>(2)));
-
-        td3.data_object("/bla", true).add_attribute("where/xsize", QVariant(static_cast<int64_t>(3)));
-        td3.data_object("/bla2", true).add_attribute("where/xsize", QVariant(static_cast<int64_t>(3)));
-
-        db.save_file("test_data_1", td1);
-        db.save_file("test_data_2", td2);
-        db.save_file("test_data_3", td3);
     }
 
-    ~RDB_Query_test() {
+    virtual void SetUp() {
+        td1.data_object("/do1", true).add_attribute("where/xsize", 1);
+        td1.data_object("/do1", true).add_attribute("where/ysize", 2);
+
+        td2.data_object("/do1", true).add_attribute("where/xsize", 2);
+        td2.data_object("/do1", true).add_attribute("where/ysize", 2);
+
+        td3.data_object("/do1", true).add_attribute("where/xsize", 3);
+        td3.data_object("/do2", true).add_attribute("where/xsize", 3);
+
+        td4.data_object("/do1", true).add_attribute("where/xsize", 6);
+        td4.data_object("/do1", true).add_attribute("where/ysize", 4);
+        td4.data_object("/do2", true).add_attribute("where/ysize", 5);
+
+        td5.data_object("/do1", true).add_attribute("where/xsize", 5);
+        td5.data_object("/do1", true).add_attribute("where/ysize", 2);
+        td5.data_object("/do2", true).add_attribute("where/xsize", 2);
+        td5.data_object("/do2", true).add_attribute("where/ysize", 5);
+
+        db.save_file("td1", td1);
+        db.save_file("td2", td2);
+        db.save_file("td3", td3);
+        db.save_file("td4", td4);
+        db.save_file("td5", td5);
+    }
+
+    virtual void TearDown() {
         db.clean();
     }
 
     expr::Factory xpr;
-    std::string src;
+    std::string src1, src2;
     RelationalDatabase db;
-    File td1, td2, td3;
+    File td1, td2, td3, td4, td5;
     Query query;
 };
 
@@ -62,90 +95,115 @@ TEST_F(RDB_Query_test, test_simple) {
                  .filter(xpr.attribute("where/xsize")->eq(xpr.integer(1)))
                  .execute();
     ASSERT_TRUE(r->next());
-    EXPECT_EQ(r->string(0), "test_data_1");
+    EXPECT_EQ(r->string(0), "td1");
     ASSERT_TRUE(not r->next());
+}
+
+typedef std::vector<std::string> StringVec;
+
+StringVec
+extract_strings_at(ResultSet& r, int pos) {
+    std::vector<std::string> vec;
+    while (r.next()) {
+        vec.push_back(r.string(pos));
+    }
+    return vec;
+}
+
+template<typename C, typename V>
+bool contains(const C& container, const V& val) {
+    return std::find(container.begin(), container.end(), val) != container.end();
 }
 
 TEST_F(RDB_Query_test, test_list_all_files) {
     shared_ptr<ResultSet> r = query.fetch(xpr.attribute("path")).execute();
-    ASSERT_EQ(r->size(), 3);
-    ASSERT_TRUE(r->next());
-    EXPECT_EQ(r->string(0), "test_data_1");
-    ASSERT_TRUE(r->next());
-    EXPECT_EQ(r->string(0), "test_data_2");
-    ASSERT_TRUE(r->next());
-    EXPECT_EQ(r->string(0), "test_data_3");
-    ASSERT_TRUE(not r->next());
+
+    EXPECT_EQ(r->size(), 5);
+    StringVec v = extract_strings_at(*r, 0);
+
+    EXPECT_TRUE(contains(v, "td1"));
+    EXPECT_TRUE(contains(v, "td2"));
+    EXPECT_TRUE(contains(v, "td3"));
+    EXPECT_TRUE(contains(v, "td4"));
+    EXPECT_TRUE(contains(v, "td5"));
 }
 
-TEST_F(RDB_Query_test, test_select_xsize) {
-    shared_ptr<ResultSet> r = query.fetch(xpr.attribute("where/xsize"))
-                       .filter(xpr.attribute("where/xsize")->eq(xpr.integer(1)))
-                       .execute();
-    ASSERT_TRUE(r->next());
-    EXPECT_EQ(r->integer(0), 1);
-    ASSERT_TRUE(not r->next());
+TEST_F(RDB_Query_test, test_filter_by_object) {
+    shared_ptr<ResultSet> r =
+        query.fetch(xpr.attribute("path"))
+             .filter(xpr.attribute("what/object")->eq(xpr.string("PVOL")))
+             .execute();
+
+    EXPECT_EQ(r->size(), 3);
+    StringVec v = extract_strings_at(*r, 0);
+
+    EXPECT_TRUE(contains(v, "td1"));
+    EXPECT_TRUE(contains(v, "td2"));
+    EXPECT_TRUE(contains(v, "td3"));
 }
 
-TEST_F(RDB_Query_test, test_select_on_double_attr) {
+TEST_F(RDB_Query_test, test_fetch_xsize_filtering_by_xsize) {
+    shared_ptr<ResultSet> r =
+        query.fetch(xpr.attribute("where/xsize"))
+             .filter(xpr.attribute("where/xsize")->eq(xpr.integer(2)))
+             .execute();
+    EXPECT_EQ(r->size(), 2);
+}
+
+TEST_F(RDB_Query_test, test_filter_by_xsize_or_ysize) {
     expr::AttributePtr xsize = xpr.attribute("where/xsize");
     expr::AttributePtr ysize = xpr.attribute("where/ysize");
-    shared_ptr<ResultSet> r = query.fetch(xpr.attribute("path"))
-                       .filter(xsize->eq(xpr.integer(1))->or_(ysize->eq(xpr.integer(2))))
-                       .execute();
-    ASSERT_EQ(r->size(), 2);
-    r->next();
-    std::string first = r->string(0);
-    r->next();
-    std::string second = r->string(0);
-    EXPECT_NE(first, second);
-}
-
-TEST_F(RDB_Query_test, test_select_not_distinct) {
-    expr::AttributePtr xsize = xpr.attribute("where/xsize");
     shared_ptr<ResultSet> r =
-            query.fetch(xpr.attribute("path"))
-                 .filter(xsize->eq(xpr.integer(3)))
-                 .execute();
-    ASSERT_EQ(r->size(), 2);
-    r->next();
-    std::string first = r->string(0);
-    r->next();
-    std::string second = r->string(0);
-    EXPECT_EQ(first, second);
+        query.fetch(xpr.attribute("path"))
+             .filter(xsize->eq(xpr.integer(1))->or_(ysize->eq(xpr.integer(2))))
+             .execute();
+
+    EXPECT_EQ(r->size(), 3);
+    StringVec v = extract_strings_at(*r, 0);
+
+    EXPECT_TRUE(contains(v, "td1"));
+    EXPECT_TRUE(contains(v, "td2"));
+    EXPECT_TRUE(contains(v, "td5"));
 }
 
-TEST_F(RDB_Query_test, test_select_distinct) {
+TEST_F(RDB_Query_test, test_filter_by_xsize_distinct) {
     expr::AttributePtr xsize = xpr.attribute("where/xsize");
     shared_ptr<ResultSet> r = 
-            query.fetch(xpr.attribute("path"))
-                 .distinct(true)
-                 .filter(xsize->eq(xpr.integer(3)))
-                 .execute();
-    ASSERT_EQ(r->size(), 1);
-    r->next();
-    EXPECT_EQ("test_data_3", r->string(0));
-}
-
-
-TEST_F(RDB_Query_test, test_select_xsize2) {
-    expr::AttributePtr xsize = xpr.attribute("where/xsize");
-    shared_ptr<ResultSet> r =
-            query.fetch(xsize)
-                 .filter(xsize->eq(xpr.integer(1))->or_(xsize->eq(xpr.integer(2))))
-                 .execute();
-    ASSERT_EQ(r->size(), 2);
-
+        query.fetch(xpr.attribute("path"))
+             .distinct(true)
+             .filter(xsize->eq(xpr.integer(3)))
+             .execute();
+    EXPECT_EQ(r->size(), 1);
+    ASSERT_TRUE(r->next());
+    EXPECT_EQ(r->string(0), "td3");
 }
 
 TEST_F(RDB_Query_test, test_select_by_wmo_code) {
     expr::AttributePtr wmo_code = xpr.attribute("src_WMO");
     shared_ptr<ResultSet> r =
-            query.fetch(xpr.attribute("path"))
-                 .filter(wmo_code->eq(xpr.integer(2606)))
-                 .execute();
-    ASSERT_EQ(r->size(), 3);
+        query.fetch(xpr.attribute("path"))
+             .filter(wmo_code->eq(xpr.integer(2666)))
+             .execute();
 
+    EXPECT_EQ(r->size(), 2);
+    StringVec v = extract_strings_at(*r, 0);
+
+    EXPECT_TRUE(contains(v, "td2"));
+    EXPECT_TRUE(contains(v, "td4"));
+}
+
+TEST_F(RDB_Query_test, test_select_by_place) {
+    shared_ptr<ResultSet> r =
+        query.fetch(xpr.attribute("path"))
+             .filter(xpr.attribute("src_PLC")->eq(xpr.string("Ängelholm")))
+             .execute();
+
+    EXPECT_EQ(r->size(), 3);
+    StringVec v = extract_strings_at(*r, 0);
+
+    EXPECT_TRUE(contains(v, "td1"));
+    EXPECT_TRUE(contains(v, "td3"));
+    EXPECT_TRUE(contains(v, "td5"));
 }
 
 #endif // BRFC_TEST_DB_DSN
