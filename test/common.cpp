86dfef1eb6b1a3af4379077945e370244e2299f9
Sander Ernes <sander.ernes@emhi.ee>
2010-02-10 15:27:56 +0200

    add ostream opeators for QDate, QTime and Variant

--- test/common.cpp
+++ test/common.cpp
@@ -1,6 +1,9 @@
 #include "common.hpp"
 
 #include <QtCore/QVariant>
+#include <QtCore/QDate>
+#include <QtCore/QTime>
+#include <brfc/Variant.hpp>
 #include <iostream>
 
 std::ostream& operator<<(std::ostream& out, const QVariant& value) {
@@ -8,3 +11,17 @@ std::ostream& operator<<(std::ostream& out, const QVariant& value) {
     return out;
 }
 
+std::ostream& operator<<(std::ostream& out, const QDate& value) {
+    out << value.toString().toStdString();
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const QTime& value) {
+    out << value.toString().toStdString();
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, const brfc::Variant& value) {
+    out << value.to_qvariant().toString().toStdString();
+    return out;
+}
--- test/common.hpp
+++ test/common.hpp
@@ -4,8 +4,16 @@
 #include <iosfwd>
 
 class QVariant;
+class QDate;
+class QTime;
 
+namespace brfc {
+    class Variant;
+}
 
 std::ostream& operator<<(std::ostream& out, const QVariant& value);
+std::ostream& operator<<(std::ostream& out, const QDate& value);
+std::ostream& operator<<(std::ostream& out, const QTime& value);
+std::ostream& operator<<(std::ostream& out, const brfc::Variant& value);
 
 #endif // BRFC_TEST_COMMON_HPP
