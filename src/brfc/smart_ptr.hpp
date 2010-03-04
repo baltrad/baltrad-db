14da81cd4c83e1f9ed5b302bac928cd0875c2e69
Sander Ernes <sander@baltrad.eu>
2010-03-04 14:48:50 +0200

    add boost::{weak_ptr|make_shared} to smart_ptr.hpp

--- src/brfc/smart_ptr.hpp
+++ src/brfc/smart_ptr.hpp
@@ -1,18 +1,25 @@
 #ifndef BRFC_SMART_PTR_HPP
 #define BRFC_SMART_PTR_HPP
 
+#include <memory>
+
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
-
-#include <memory>
+#include <boost/weak_ptr.hpp>
 
 namespace brfc {
 
 using std::auto_ptr;
+
 using boost::scoped_ptr;
 using boost::shared_ptr;
+using boost::weak_ptr;
+
 using boost::enable_shared_from_this;
+using boost::make_shared;
+
 using boost::const_pointer_cast;
 using boost::dynamic_pointer_cast;
 using boost::static_pointer_cast;
