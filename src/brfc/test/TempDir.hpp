7f4d2cc6a3a3da0ef4b97763a2db07afb49cb73a
Sander Ernes <sander@baltrad.eu>
2010-04-14 12:06:09 +0300

    remove excessive space from license

--- src/brfc/test/TempDir.hpp
+++ src/brfc/test/TempDir.hpp
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with baltrad-db.  If not, see <http://www.gnu.org/licenses/>.
+along with baltrad-db. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef BRFC_TEST_TEMP_DIR_HPP
--- src/brfc/test/TestRDB.cpp
+++ src/brfc/test/TestRDB.cpp
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with baltrad-db.  If not, see <http://www.gnu.org/licenses/>.
+along with baltrad-db. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <brfc/test/TestRDB.hpp>
