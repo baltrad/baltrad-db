3a68ebae694f0abd87f789ffc66fcf41c41ad597
Sander Ernes <sander@baltrad.eu>
2011-04-14 16:49:05 +0300

    add binary arithmetic sexp operators

---
+++ src/brfc/expr/proc/arithmetic.hpp
@@ -0,0 +1,123 @@
+/*
+Copyright 2010-2011 Estonian Meteorological and Hydrological Institute
+
+This file is part of baltrad-db.
+
+baltrad-db is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+baltrad-db is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with baltrad-db. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BRFC_EXPR_PROC_ARITHMETIC_HPP
+#define BRFC_EXPR_PROC_ARITHMETIC_HPP
+
+#include <boost/mpl/and.hpp>
+#include <boost/type_traits/is_arithmetic.hpp>
+
+#include <brfc/assert.hpp>
+#include <brfc/expr/sexp.hpp>
+
+namespace brfc {
+namespace expr {
+namespace proc {
+
+// + could additionally be defined for string + string, list + list
+struct add : public static_visitor<sexp> {
+    template<typename T, typename U>
+    sexp dispatch(const T&, const U&, boost::mpl::false_) const {
+        BRFC_ASSERT(false);
+        return sexp();
+    }
+    
+    template<typename T, typename U>
+    sexp dispatch(const T& lhs, const U& rhs, boost::mpl::true_) const {
+        return sexp(lhs + rhs);
+    }
+
+    template<typename T, typename U>
+    sexp operator()(const T& lhs, const U& rhs) const {
+        return dispatch(lhs, rhs,
+                        boost::mpl::and_<
+                            boost::is_arithmetic<T>,
+                            boost::is_arithmetic<U> >());
+    }
+};
+
+struct sub : public static_visitor<sexp> {
+    template<typename T, typename U>
+    sexp dispatch(const T&, const U&, boost::mpl::false_) const {
+        BRFC_ASSERT(false);
+        return sexp();
+    }
+    
+    template<typename T, typename U>
+    sexp dispatch(const T& lhs, const U& rhs, boost::mpl::true_) const {
+        return sexp(lhs - rhs);
+    }
+
+    template<typename T, typename U>
+    sexp operator()(const T& lhs, const U& rhs) const {
+        return dispatch(lhs, rhs,
+                        boost::mpl::and_<
+                            boost::is_arithmetic<T>,
+                            boost::is_arithmetic<U> >());
+    }
+};
+
+struct mul : public static_visitor<sexp> {
+    template<typename T, typename U>
+    sexp dispatch(const T&, const U&, boost::mpl::false_) const {
+        BRFC_ASSERT(false);
+        return sexp();
+    }
+    
+    template<typename T, typename U>
+    sexp dispatch(const T& lhs, const U& rhs, boost::mpl::true_) const {
+        return sexp(lhs * rhs);
+    }
+
+    template<typename T, typename U>
+    sexp operator()(const T& lhs, const U& rhs) const {
+        return dispatch(lhs, rhs,
+                        boost::mpl::and_<
+                            boost::is_arithmetic<T>,
+                            boost::is_arithmetic<U> >());
+    }
+};
+
+
+struct div : public static_visitor<sexp> {
+    template<typename T, typename U>
+    sexp dispatch(const T&, const U&, boost::mpl::false_) const {
+        BRFC_ASSERT(false);
+        return sexp();
+    }
+    
+    template<typename T, typename U>
+    sexp dispatch(const T& lhs, const U& rhs, boost::mpl::true_) const {
+        return sexp(lhs / rhs);
+    }
+
+    template<typename T, typename U>
+    sexp operator()(const T& lhs, const U& rhs) const {
+        return dispatch(lhs, rhs,
+                        boost::mpl::and_<
+                            boost::is_arithmetic<T>,
+                            boost::is_arithmetic<U> >());
+    }
+};
+
+} // namespace proc
+} // namespace expr
+} // namespace brfc
+
+#endif // BRFC_EXPR_PROC_ARITHMETIC_HPP
---
+++ test/brfc/expr/proc/arithmetic_test.cpp
@@ -0,0 +1,82 @@
+/*
+Copyright 2010-2011 Estonian Meteorological and Hydrological Institute
+
+This file is part of baltrad-db.
+
+baltrad-db is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+baltrad-db is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with baltrad-db. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtest/gtest.h>
+
+#include <stdexcept>
+
+#include <brfc/expr/proc/arithmetic.hpp>
+
+namespace brfc {
+namespace expr {
+namespace proc {
+
+TEST(expr_proc_arithmetic_add_test, test_int_int) {
+    EXPECT_EQ(sexp(3), add()(1, 2));
+}
+
+TEST(expr_proc_arithmetic_add_test, test_int_float) {
+    EXPECT_EQ(sexp(3.5), add()(1, 2.5));
+}
+
+TEST(expr_proc_arithmetic_add_test, test_int_list) {
+    EXPECT_THROW(add()(1, sexp::list_t()), std::logic_error);
+}
+
+TEST(expr_proc_arithmetic_sub_test, test_int_int) {
+    EXPECT_EQ(sexp(-1), sub()(1, 2));
+}
+
+TEST(expr_proc_arithmetic_sub_test, test_int_float) {
+    EXPECT_EQ(sexp(-1.5), sub()(1, 2.5));
+}
+
+TEST(expr_proc_arithmetic_sub_test, test_int_list) {
+    EXPECT_THROW(sub()(1, sexp::list_t()), std::logic_error);
+}
+
+TEST(expr_proc_arithmetic_mul_test, test_int_int) {
+    EXPECT_EQ(sexp(6), mul()(2, 3));
+}
+
+TEST(expr_proc_arithmetic_mul_test, test_int_float) {
+    EXPECT_EQ(sexp(5.0), mul()(2, 2.5));
+}
+
+TEST(expr_proc_arithmetic_mul_test, test_int_list) {
+    EXPECT_THROW(mul()(1, sexp::list_t()), std::logic_error);
+}
+
+TEST(expr_proc_arithmetic_div_test, test_int_int) {
+    EXPECT_EQ(sexp(0), div()(2, 3));
+    EXPECT_EQ(sexp(1), div()(3, 2));
+}
+
+TEST(expr_proc_arithmetic_div_test, test_int_float) {
+    EXPECT_EQ(sexp(2), div()(5, 2.5));
+    EXPECT_EQ(sexp(3.5), div()(7.0, 2));
+}
+
+TEST(expr_proc_arithmetic_div_test, test_int_list) {
+    EXPECT_THROW(div()(1, sexp::list_t()), std::logic_error);
+}
+
+} // namespace proc
+} // namespace expr
+} // namespace brfc
