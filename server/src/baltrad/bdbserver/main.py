fd0729ef0fad0c775a44fff199cdee314f2170be
Sander Ernes <sander@baltrad.eu>
2012-01-05 09:52:39 +0200

    Ticket 746: add option to log into file for the bdb server executable

--- server/src/baltrad/bdbserver/main.py
+++ server/src/baltrad/bdbserver/main.py
@@ -3,6 +3,8 @@ import optparse
 import os
 import sys
 
+from copy import copy
+
 import daemon
 from daemon.pidfile import TimeoutPIDLockFile
 
@@ -10,16 +12,23 @@ from .config import Properties
 from .web.app import Application, serve
 from . import backend
 
+def check_path(option, opt, value):
+    return os.path.abspath(value)
+
+class Option(optparse.Option):
+    TYPES = optparse.Option.TYPES + ("path",)
+    TYPE_CHECKER = copy(optparse.Option.TYPE_CHECKER)
+    TYPE_CHECKER["path"] = check_path
+
 def create_optparser():
-    optparser = optparse.OptionParser()
+    optparser = optparse.OptionParser(option_class=Option)
     optparser.set_usage(
         "%s [--conf=CONFFILE] [ARGS]" % (
             os.path.basename(sys.argv[0])
         )
     )
     optparser.add_option(
-        "--conf", dest="conffile",
-        default="http://localhost:8080",
+        "--conf", type="path", dest="conffile",
         help="configuration file",
     )
     return optparser
@@ -64,11 +73,17 @@ def run_upgrade():
 
     be = backend.create_from_config(config)
     be.upgrade()
+
+def configure_logging(opts):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if opts.foreground:
+        logger.addHandler(logging.StreamHandler(sys.stdout))
+    if opts.logfile:
+        logger.addHandler(logging.FileHandler(opts.logfile))
     
 def run_server():
-    logging.basicConfig()
-    logging.getLogger("werkzeug").setLevel(logging.INFO)
-
     optparser = create_optparser()
     optparser.add_option(
         "--foreground", action="store_true",
@@ -76,13 +91,16 @@ def run_server():
         help="don't detach the process"
     )
     optparser.add_option(
-        "--pidfile", action="store",
+        "--logfile", type="path", action="store",
+        help="location of the log file",
+    )
+    optparser.add_option(
+        "--pidfile", type="path", action="store",
         default="/var/run/baltrad-bdb-server.pid",
         help="location of the pid file"
     )
 
     opts, args = optparser.parse_args()
-
 
     config = read_config(opts.conffile)
     config = config.filter("baltrad.bdb.server.")
@@ -97,6 +115,7 @@ def run_server():
     )
 
     with daemon_ctx:
+        configure_logging(opts)
         be = backend.create_from_config(config)
         if not be.is_operational():
             raise SystemExit("backend is not operational")
