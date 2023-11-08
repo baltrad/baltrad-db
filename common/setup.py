ea62fd8f47c4f1fe398e70605092815a2380cd62
Anders Henja <anders.henja@gmail.com>
2023-11-08 13:33:36 +0100

    Added different optional requirement of h5py for bdbcommon

--- common/setup.py
+++ common/setup.py
@@ -2,6 +2,14 @@
 
 import setuptools
 
+REQUIRED_PACKAGES=[]
+
+# If we can't get hold of pyhl we install h5py instead
+try:
+    import _pyhl
+except ModuleNotFoundError:
+    REQUIRED_PACKAGES.append("h5py >= 3.6, < 4.0")
+    
 setuptools.setup(name="baltrad.bdbcommon",
     version="0.1-dev",
     namespace_packages=["baltrad"],
@@ -13,9 +21,7 @@ setuptools.setup(name="baltrad.bdbcommon",
     package_dir={
         "": "src"
     },
-    install_requires=[
-#        "hlhdf",
-    ],
+    install_requires=REQUIRED_PACKAGES,
     test_suite="nose.collector",
     tests_require=[
         "mock >= 0.7",
